5c94d41d8bfa4a4c87b04c0906ad36a93c874176
Marek Witkowski <mr.marek.witkowski@gmail.com>
2024-06-11 14:36:06 +0200

    chore: clean up

--- lib/specimen/version.rb
+++ lib/specimen/version.rb
@@ -1,7 +1,6 @@
 # frozen_string_literal: true
 
 module Specimen
-  # Returns the currently loaded version of \Rails as a +Gem::Version+.
   def self.gem_version
     Gem::Version.new VERSION::STRING
   end
@@ -10,9 +9,8 @@ module Specimen
     MAJOR = 0
     MINOR = 0
     TINY  = 1
-    PRE   = "alpha.b"
+    PRE   = 'alpha.b'
 
-    STRING = [MAJOR, MINOR, TINY, PRE].compact.join(".")
+    STRING = [MAJOR, MINOR, TINY, PRE].compact.join('.')
   end
-
 end
