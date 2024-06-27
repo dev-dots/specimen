bf5fe3b935edb1cc346672001618e0c404308e49
Marek Witkowski <mr.marek.witkowski@gmail.com>
2024-06-27 20:44:52 +0200

    feat: add Runtime module

--- lib/specimen.rb
+++ lib/specimen.rb
@@ -8,11 +8,16 @@ require 'pry'
 require 'specimen/extensions/ruby/hash'
 
 require 'specimen/command'
+require 'specimen/runtime'
 require 'specimen/version'
-
 
 module Specimen
   extend ActiveSupport::Autoload
 
   autoload :Config
+  autoload :Runtime
+
+  class << self
+    attr_accessor :runtime
+  end
 end
